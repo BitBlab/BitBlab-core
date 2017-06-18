49ce1b8ddf9cffa3e0b532684b268d61a21d8a73
Amarite1 <me@t3kbau5.com>
2017-06-18 14:38:49 -0400

    build: switch bcrypt to bcryptjs
    The bcrypt package is buggy, so this commit replaces it with bcryptjs. This
    module uses the same API but is purely JS-based.

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var path = require('path');
 var njscrypto = require('node-cryptojs-aes');
 var CryptoJS = njscrypto.CryptoJS;
 
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 
 var app = express();
 
